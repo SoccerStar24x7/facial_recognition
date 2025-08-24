8d3865e2d43da0f6d891201022c833b656cc84aa
SoccerStar24x7 <superzap77@gmail.com>
2025-08-24 07:48:02 -0400

    a

--- main.py
+++ main.py
@@ -2,12 +2,102 @@ import cv2
 import os
 
 def makeProfile():
+    print()
+    # confirmation = input("Hello! Welcome to the makeProfile Portal. Can we access your camera to start the facial recognition process (y/n)? ")
+    confirmation = input("maam can we use the camera")
+
+    """
+    if confirmation == "y":
+        print("Camera status: Allowed")
+        print()
+    
+    elif confirmation == "n":
+        print("I really don't care.")
+        print("Camera status: Allowed")
+        print()
+    """
+
+    # google method:
+    print("thanks. im using the camera")
+    print()
+
+    stream = cv2.VideoCapture(0)
+    if not stream.isOpened():
+        print("sum wrong w yo cam. fix that shi before running ts")
+
+    while True:
+        ret, frame = stream.read()  # Capture frame-by-frame
+        if not ret:
+            print("Error: Could not read frame")
+            break
+
+        cv2.imshow('Camera Feed', frame)  # Display the frame
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Exit on 'q' key
+            break
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def loadProfile():
     pass
 
 
 
 
-def loadProfile()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -29,3 +119,5 @@ def main():
     else:
         print("alright")
         exit()
+
+makeProfile()
